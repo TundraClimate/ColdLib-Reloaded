49b3cad9c62453ad99c18862af94b7b7f2ccee4b
TundraClimate <tundra.tundere@gmail.com>
2022-10-17 23:29:51 +0900

    add feat

---
+++ src/main/kotlin/io/github/tundraclimate/clib/db/BufferedSQLiteProvider.kt
@@ -0,0 +1,42 @@
+package io.github.tundraclimate.clib.db
+
+class BufferedSQLiteProvider(private val sqlite: SQLite) {
+    private val buff = mutableListOf<Pair<String, List<String>?>>()
+
+    fun executeCreate(type: CreateType) {
+        val sql = "CREATE $type"
+        buff.add(sql to null)
+    }
+
+    fun executeInsert(table: String, vararg values: String) {
+        val sql = "INSERT INTO $table VALUES(${values.reduce { it, _ -> "$it, ?" }})"
+        buff.add(sql to values.toList())
+    }
+
+    fun executeUpdate(table: String, where: String = "", vararg newValues: String) {
+        val sql = "UPDATE $table SET ${newValues.reduce { it, _ -> "$it, ?" }} $where"
+        buff.add(sql to newValues.toList())
+    }
+
+    fun executeDelete(table: String, where: String = "") {
+        val sql = "DELETE FROM $table $where"
+        buff.add(sql to null)
+    }
+
+    fun flush() {
+        sqlite.exec { _, state ->
+            buff.forEach {
+                if (it.second != null) {
+                    sqlite.preparedState(it.first) { pres ->
+                        it.second!!.forEachIndexed { index, n ->
+                            pres.setString(index + 1, n)
+                        }
+                        pres.executeUpdate()
+                    }
+                } else {
+                    state.executeUpdate(it.first)
+                }
+            }
+        }
+    }
+}
